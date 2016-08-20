d1f02288187d8584f15e140db8683f377f1f74cb
karthick1995 <karthickcsea50@gmail.com>
2016-08-20 13:44:43 +0530

    Update leastdig.java

--- leastdig.java
+++ leastdig.java
@@ -5,27 +5,34 @@ class leastdig
   {
   Scanner sc=new Scanner(System.in);
   System.out.println("Enter the input1: ");
-  int n=sc.nextInt();
+  int n=sc.nextInt();String s=n+"";
   System.out.println("Enter the K digit value: ");
   int k=sc.nextInt();
+  if(s.length()>k)
+  {
+    int m=0;
   TreeSet ts=new TreeSet();
   while(n!=0)
   {
    int j=n%10;
     ts.add(j);
-    n=n/10;
+    n=n/10;m=m+1;
   }
   Iterator<Integer> i=ts.iterator();int check=0;
-  while(i.hasNext())
-  {
-    if(check<k)
+  k=m-k;
+   while(i.hasNext())
     {
-      check+=1;
-      System.out.print(i.next());
+     if(check<k)
+     {
+        check+=1;
+       System.out.print(i.next());
+      }
+      else
+      break;
     }
-    else
-    break;
   }
+  else
+  System.out.println("Wrong K digit");
   }
 
 }
